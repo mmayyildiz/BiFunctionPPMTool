cb5b1eff9b055f0acccd1f0c597b4cbc4cead194
meltem ayyıldız <mmeltemayyildiz@gmail.com>
2019-09-14 14:25:58 +0300

    deleted

---
+++ PPMToolFullStack/src/main/java/com/bifunction/ppmtool/PpmToolFullStackApplication.java
@@ -0,0 +1,13 @@
+package com.bifunction.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolFullStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolFullStackApplication.class, args);
+	}
+
+}
---
+++ PPMToolFullStack/src/test/java/com/bifunction/ppmtool/PpmToolFullStackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bifunction.ppmtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PpmToolFullStackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
